2aa83e3fcbd3af2fdda2172895c0d193ad9d31f5
Bildan Jauhary <bildanjauhary@gmail.com>
2026-07-06 15:20:50 +0700

    feat: add task 4 about struct and slice

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func task1() {
+
+	// declares all needed structs
 	type str struct {
 		the string
 	}
@@ -19,11 +23,9 @@ func task1() {
 	best := strbest{
 		best: "Koda",
 	}
-
 	the := strthe{
 		the: best,
 	}
-
 	we := strSt{
 		are: the,
 	}
@@ -48,8 +50,59 @@ func task5() {
 	fmt.Println(nums[1] + nums[2])
 }
 
+func task4() {
+
+	// declares all needed structs
+	type isSt struct {
+		is string
+	}
+	type fruitsSt struct {
+		fruit isSt
+	}
+	type fav struct {
+		favorite [4]fruitsSt
+	}
+
+	// the values of all struct of fruits
+	isApple := isSt{
+		is: "Apple",
+	}
+	isDurian := isSt{
+		is: "Durian",
+	}
+	isManggo := isSt{
+		is: "Manggo",
+	}
+	isPineapple := isSt{
+		is: "Pineapple",
+	}
+
+	// the structs of all fruits
+	apple := fruitsSt{
+		fruit: isApple,
+	}
+	manggo := fruitsSt{
+		fruit: isManggo,
+	}
+	durian := fruitsSt{
+		fruit: isDurian,
+	}
+	pineapple := fruitsSt{
+		fruit: isPineapple,
+	}
+
+	favorite := fav{
+		favorite: [4]fruitsSt{manggo, durian, pineapple, apple},
+	}
+
+	my := []fav{favorite}
+
+	fmt.Println(my[0].favorite[3].fruit.is)
+}
+
 func main() {
 	task1()
 	task2()
 	task5()
+	task4()
 }
