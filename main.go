913bfda19cbeab665651022cdcf5dd37435f475e
Bildan Jauhary <bildanjauhary@gmail.com>
2026-07-06 21:24:14 +0700

    refactor: refactoring declaration struct

--- main.go
+++ main.go
@@ -4,22 +4,21 @@ import (
 	"fmt"
 )
 
+// declares all needed structs
+type str struct {
+	the string
+}
+type strbest struct {
+	best string
+}
+type strthe struct {
+	the strbest
+}
+type strSt struct {
+	are strthe
+}
+
 func task1() {
-
-	// declares all needed structs
-	type str struct {
-		the string
-	}
-	type strbest struct {
-		best string
-	}
-	type strthe struct {
-		the strbest
-	}
-	type strSt struct {
-		are strthe
-	}
-
 	best := strbest{
 		best: "Koda",
 	}
@@ -33,36 +32,72 @@ func task1() {
 	fmt.Println(we.are.the.best)
 }
 
-func task2() {
-	type str struct {
-		world string
-	}
+type strWorld struct {
+	world string
+}
 
-	hello := str{
+func task2() {
+	hello := strWorld{
 		world: "Hello World",
 	}
-
 	fmt.Println(hello.world)
 }
 
-func task5() {
-	nums := [3]int{4, 20, 12}
-	fmt.Println(nums[1] + nums[2])
+// func task3() {
+// 	type techSt struct {
+// 		academy string
+// 	}
+
+// 	type manSt struct {
+// 		tech techSt
+// 	}
+
+// 	type structArr struct {
+// 		man manSt
+// 	}
+
+// 	type str struct {
+// 		tech techSt
+// 	}
+
+// 	tech := techSt {
+// 		academy: "Tech Academy",
+// 	}
+
+// 	man := manSt {
+// 		tech: tech,
+// 	}
+
+//  structA:= []structArr{{}, {}}
+
+//  type objStr struct {
+// 	str [][][]kodaSt
+//  }
+
+//  strArr = []objStr {{}, {}, {}, {}}
+
+//  var objSlice = [][]str {null, null, {structA[0].tech.academy},}
+
+//  var strSlice = [][][]str{{}, {}, {null, structA}}
+
+//  obj := objStr {
+// 	str: structA,
+//  }
+
+// }
+
+// declares all needed structs
+type isSt struct {
+	is string
+}
+type fruitsSt struct {
+	fruit isSt
+}
+type fav struct {
+	favorite [4]fruitsSt
 }
 
 func task4() {
-
-	// declares all needed structs
-	type isSt struct {
-		is string
-	}
-	type fruitsSt struct {
-		fruit isSt
-	}
-	type fav struct {
-		favorite [4]fruitsSt
-	}
-
 	// the values of all struct of fruits
 	isApple := isSt{
 		is: "Apple",
@@ -100,9 +135,14 @@ func task4() {
 	fmt.Println(my[0].favorite[3].fruit.is)
 }
 
+func task5() {
+	nums := [3]int{4, 20, 12}
+	fmt.Println(nums[1] + nums[2])
+}
+
 func main() {
 	task1()
 	task2()
-	task5()
 	task4()
+	task5()
 }
